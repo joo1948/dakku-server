2e792983c85ac0d928b5ab32818e11ad87d86277
jykang <joo.zero.10@gmail.com>
2022-09-07 10:08:56 +0900

    등록/수정자 코드 처리

--- src/main/java/com/mirim9/dakku/DakkuServerApplication.java
+++ src/main/java/com/mirim9/dakku/DakkuServerApplication.java
@@ -2,7 +2,12 @@ package com.mirim9.dakku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,5 +16,7 @@ public class DakkuServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DakkuServerApplication.class, args);
 	}
+
+
 
 }

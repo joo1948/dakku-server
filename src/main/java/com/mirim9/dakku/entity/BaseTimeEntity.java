47b88d4369da57a20d85d9f8bdcf273ae9b6e2b1
jykang <joo.zero.10@gmail.com>
2022-09-07 10:20:13 +0900

    등록/수정자 코드 처리

--- src/main/java/com/mirim9/dakku/entity/BaseTimeEntity.java
+++ src/main/java/com/mirim9/dakku/entity/BaseTimeEntity.java
@@ -1,12 +1,11 @@
 package com.mirim9.dakku.entity;
 
+
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -14,15 +13,11 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity extends BaseTimeEntity{
+public abstract class BaseTimeEntity {
 
-    //등록자/수정자 등의 사용자 코드 넣을 시 Security사용하여 Session처리해줄 것.
-    @CreatedBy
-    @Column(updatable = false)
-    private String createdBy;
+    @CreatedDate
+    private LocalDateTime createDateTime;
 
-    @CreatedBy
-    @Column(updatable = false)
-    private String modifiedBy;
-
+    @LastModifiedDate
+    private LocalDateTime modifyDateTime;
 }
